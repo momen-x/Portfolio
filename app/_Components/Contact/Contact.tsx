17ae18629ba7fc4577c325c4749cd1d4d8286bb1
Mo'men <mmm.isam.99@gmail.com>
2025-11-15 16:58:47 +0200

    edit some feature

--- app/_Components/Contact/Contact.tsx
+++ app/_Components/Contact/Contact.tsx
@@ -20,9 +20,9 @@ const contactWayList = [
   },
   {
     appName: "contact.email",
-    text: "mmm.isam.99@gmail.com",
+    text: "moamenalswafiri@gmail.com",
     path: "M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z",
-    link: "mailto:mmm.isam.99@gmail.com",
+    link: "mailto:malswafiri@gmail.com",
     directmessage: "contact.emailMessage",
     message: "contact.emailSendMessage",
     color: "blue",
